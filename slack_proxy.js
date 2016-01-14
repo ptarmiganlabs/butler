427aa45754853009b7a4f6d44a64c576fb99ac57
mountaindude <mountaindude@ptarmiganlabs.com>
2016-01-14 21:01:58 +0100

    Added crate directory capability
    A new endpoint, /createDir, makes it possible to create directories in
    the file system of the server on which SlackProxy is running.

--- slack_proxy.js
+++ slack_proxy.js
@@ -1,15 +1,14 @@
 var restify = require('restify');
 var Slack = require('node-slack');
-
+var mkdirp = require('mkdirp');
 
 
 var slackWebhookURL = '<fill in your web hook URL from Slack>';
 var slack = new Slack(slackWebhookURL);
 
 
-
 function respondSlack(req, res, next) {
-//  console.log(req.params);
+  console.log(req.params);
 
   slack.send({
       text: req.params.msg,
@@ -22,6 +21,20 @@ function respondSlack(req, res, next) {
   next();
 }
 
+function respondCreateDir(req, res, next) {
+  console.log(req.params);
+
+  mkdirp(req.params.directory, function(err) {
+    // path was created unless there was error
+    console.log('created dir ' + req.params.directory);
+  });
+
+  res.send(req.params);
+  next();
+}
+
+
+
 var server = restify.createServer({
   name: 'SlackProxy'
 });
@@ -29,6 +42,7 @@ var server = restify.createServer({
 server.use(restify.queryParser());    // Enable parsing of http parameters
 
 server.get('/slack', respondSlack);
+server.get('/createDir', respondCreateDir);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
