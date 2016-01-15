10d1f76e1b876ce5feb9dab2c4766b1442162c24
mountaindude <mountaindude@ptarmiganlabs.com>
2016-01-15 11:12:03 +0100

    Added endpoint for getting disk space

--- slack_proxy.js
+++ slack_proxy.js
@@ -1,7 +1,7 @@
 var restify = require('restify');
 var Slack = require('node-slack');
 var mkdirp = require('mkdirp');
-
+var disk = require('diskusage');
 
 var slackWebhookURL = '<fill in your web hook URL from Slack>';
 var slack = new Slack(slackWebhookURL);
@@ -34,6 +34,32 @@ function respondCreateDir(req, res, next) {
 }
 
 
+function respondGetDiskSpace(req, res, next) {
+  //console.log(req.params);
+
+  // // Windows: get disk usage. Takes path as first parameter
+  // disk.check('c:', function(err, info) {
+  //   req.params.available = info.available;
+  //   req.params.free = info.free;
+  //   req.params.total = info.total;
+  // });
+
+
+  // OSX/Linux: get disk usage. Takes mount point as first parameter
+  disk.check('/', function(err, info) {
+    req.params.available = info.available;
+    req.params.free = info.free;
+    req.params.total = info.total;
+  });
+
+
+  res.send(req.params);
+  next();
+}
+
+
+
+
 
 var server = restify.createServer({
   name: 'SlackProxy'
@@ -43,6 +69,7 @@ server.use(restify.queryParser());    // Enable parsing of http parameters
 
 server.get('/slack', respondSlack);
 server.get('/createDir', respondCreateDir);
+server.get('/getDiskSpace', respondGetDiskSpace);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
