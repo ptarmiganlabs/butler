03fa7a686f08d258243967096c7575c1d75e14ef
mountaindude <mountaindude@ptarmiganlabs.com>
2016-03-26 23:45:49 +0100

    Added publish to MQTT, changed name to butler, included QSocks

--- butler.js
+++ butler.js
@@ -2,9 +2,27 @@ var restify = require('restify');
 var Slack = require('node-slack');
 var mkdirp = require('mkdirp');
 var disk = require('diskusage');
+var mqtt = require('mqtt');
+var qsocks = require('qsocks');
 
+ 
 var slackWebhookURL = '<fill in your web hook URL from Slack>';
 var slack = new Slack(slackWebhookURL);
+//var mqttClient  = mqtt.connect('mqtt://<MQTT broker>');
+var mqttClient  = mqtt.connect('mqtt://localhost');
+
+var configQSocks = {
+//    host: 'sense-demo.qlik.com',
+    host: 'sense.spotify.net',
+    isSecure: true,
+    prefix: dev
+};
+
+
+
+qsocks.Connect(configQSocks).then(function(global) {
+    console.log(global);
+})
 
 
 function respondSlack(req, res, next) {
@@ -58,11 +76,46 @@ function respondGetDiskSpace(req, res, next) {
 }
 
 
+function respondMQTTPublishMessage(req, res, next) {
+  // Use data in request to publish MQTT message
+  console.log(req.params);
+  
+  mqttClient.publish(req.params.topic, req.params.message);
+    
+  res.send(req.params);
+  next();
+}
+
+
+
+function respondMQTTStartSenseTask(req, res, next) {
+  // Use data in request to start Qlik Sense task
+  console.log(req.params);
+  
+  mqttClient.publish(req.params.topic, req.params.message);
+    
+  res.send(req.params);
+  next();
+}
+
+
+
+mqttClient.on('connect', function () {
+  console.log('Connected to MQTT broker');
+//  mqttClient.subscribe('presence');
+//  mqttClient.publish('presence', 'Hello mqtt');
+});
+ 
+mqttClient.on('message', function (topic, message) {
+  // message is Buffer 
+  console.log(message.toString());
+//  mqttClient.end();
+});
 
 
 
 var server = restify.createServer({
-  name: 'SlackProxy'
+  name: 'Butler'
 });
 
 server.use(restify.queryParser());    // Enable parsing of http parameters
@@ -70,6 +123,9 @@ server.use(restify.queryParser());    // Enable parsing of http parameters
 server.get('/slack', respondSlack);
 server.get('/createDir', respondCreateDir);
 server.get('/getDiskSpace', respondGetDiskSpace);
+server.get('/mqttPublishMessage', respondMQTTPublishMessage);
+server.get('/mqttStartSenseTask', respondMQTTPublishMessage);
+
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
