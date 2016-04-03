a29e7a291abf3d23970dc34b56d800b45e742db0
mountaindude <mountaindude@ptarmiganlabs.com>
2016-04-03 06:52:12 +0200

    Added UDP server, general cleanup

--- butler.js
+++ butler.js
@@ -4,35 +4,33 @@ var mkdirp = require('mkdirp');
 var disk = require('diskusage');
 var mqtt = require('mqtt');
 var qsocks = require('qsocks');
+var dgram = require('dgram')
 
- 
+// Set up various objects and variables needed by the app
 var slackWebhookURL = '<fill in your web hook URL from Slack>';
+const SLACK_LOGIN_NOTIFICATION_CHANNEL = '<fill in name of Slack chanel where audient events should be displayed>';
+
 var slack = new Slack(slackWebhookURL);
-//var mqttClient  = mqtt.connect('mqtt://<MQTT broker>');
 var mqttClient  = mqtt.connect('mqtt://localhost');
+var restServer = restify.createServer({
+  name: 'Qlik Sense Butler'
+});
 
-var configQSocks = {
-//    host: 'sense-demo.qlik.com',
-    host: 'sense.spotify.net',
-    isSecure: true,
-    prefix: dev
-};
+var udpServer = dgram.createSocket({type:"udp4", reuseAddr:true});
+const UDP_PORT = 9997;
+const UDP_HOST = 'localhost';
 
 
-
-qsocks.Connect(configQSocks).then(function(global) {
-    console.log(global);
-})
 
 
 function respondSlack(req, res, next) {
   console.log(req.params);
 
   slack.send({
-      text: req.params.msg,
-      channel: req.params.channel,
-      username: req.params.from_user,
-      icon_emoji: req.params.emoji
+    text: req.params.msg,
+    channel: req.params.channel,
+    username: req.params.from_user,
+    icon_emoji: req.params.emoji
   });
 
   res.send(req.params);
@@ -79,9 +77,9 @@ function respondGetDiskSpace(req, res, next) {
 function respondMQTTPublishMessage(req, res, next) {
   // Use data in request to publish MQTT message
   console.log(req.params);
-  
+
   mqttClient.publish(req.params.topic, req.params.message);
-    
+
   res.send(req.params);
   next();
 }
@@ -91,9 +89,9 @@ function respondMQTTPublishMessage(req, res, next) {
 function respondMQTTStartSenseTask(req, res, next) {
   // Use data in request to start Qlik Sense task
   console.log(req.params);
-  
+
   mqttClient.publish(req.params.topic, req.params.message);
-    
+
   res.send(req.params);
   next();
 }
@@ -105,28 +103,58 @@ mqttClient.on('connect', function () {
 //  mqttClient.subscribe('presence');
 //  mqttClient.publish('presence', 'Hello mqtt');
 });
- 
+
 mqttClient.on('message', function (topic, message) {
-  // message is Buffer 
+  // message is Buffer
   console.log(message.toString());
 //  mqttClient.end();
 });
 
 
 
-var server = restify.createServer({
-  name: 'Butler'
+restServer.use(restify.queryParser());    // Enable parsing of http parameters
+
+
+// Set up UDP server
+udpServer.on('listening', () => {
+  var address = udpServer.address();
+  console.log('UDP server listening on %s:%s', address.address, address.port);
+  mqttClient.publish('butler/udpserver/', 'listening');
 });
 
-server.use(restify.queryParser());    // Enable parsing of http parameters
-
-server.get('/slack', respondSlack);
-server.get('/createDir', respondCreateDir);
-server.get('/getDiskSpace', respondGetDiskSpace);
-server.get('/mqttPublishMessage', respondMQTTPublishMessage);
-server.get('/mqttStartSenseTask', respondMQTTPublishMessage);
-
-
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+udpServer.on('error', () => {
+  var address = udpServer.address();
+  console.log('UDP server error on %s:%s', address.address, address.port);
+  mqttClient.publish('butler/udpserver/', 'error');
 });
+
+udpServer.on('message', function(message, remote) {
+  var msg = message.toString().split(';');
+
+  slack.send({
+    text: msg[0] + ' for user ' + msg[2],
+    channel: SLACK_LOGIN_NOTIFICATION_CHANNEL,
+    username: 'goran',
+    icon_emoji: ''
+  });
+
+  mqttClient.publish('butler/udpserver/', 'slack message');
+});
+
+
+
+// Set up endpoints for REST server
+restServer.get('/slack', respondSlack);
+restServer.get('/createDir', respondCreateDir);
+restServer.get('/getDiskSpace', respondGetDiskSpace);
+restServer.get('/mqttPublishMessage', respondMQTTPublishMessage);
+restServer.get('/mqttStartSenseTask', respondMQTTPublishMessage);
+
+
+restServer.listen(8080, function() {
+//  console.log('%s REST server listening on %s', restServer.name, restServer.url);
+  console.log('REST server listening on %s', restServer.url);
+});
+
+//udpServer.bind(UDP_PORT, UDP_HOST);
+udpServer.bind(UDP_PORT);
