deb4f75fb897719eb37cae29ff4a3659a40d20d9
mountaindude <mountaindude@ptarmiganlabs.com>
2016-06-26 19:52:41 +0200

    Fixed ESLint errors and added mkdocs documentation
    - No more currently outstanding ESLint errors
    - Added MkDocs as documentation generator

---
+++ butler-docs/docs/index.md
@@ -0,0 +1,21 @@
+# Welcome to Butler documentation
+
+Butler is an open source add-on tool for Qlik Sense, extending that platform with various features, most of which are focused on integrating Sense with other systems.  
+
+All project files are hosted at stored on GitHub.
+
+Use the menu to the left to access the Butler documentation.
+
+
+If you find errors in the documentation, please [open an issue in the GitHub repository](https://github.com/mountaindude/butler/issues).  
+Or [fork the project](https://github.com/mountaindude/butler/issues#fork-destination-box) and contribute with enhancements.
+
+
+
+
+## Project layout
+
+    mkdocs.yml    # The configuration file.
+    docs/
+        index.md  # The documentation homepage.
+        ...       # Other markdown pages, images and other files.
--- src/udp/udp_handlers.js
+++ src/udp/udp_handlers.js
@@ -6,38 +6,39 @@ var globals = require('../globals');
 // Set up UDP server handlers for acting on Sense failed task events
 // --------------------------------------------------------
 module.exports.udpInitTaskErrorServer = function () {
-    // Handler for UDP server startup event
-    globals.udpServerTaskFailureSocket.on('listening', function(message, remote) {
-        var address = globals.udpServerTaskFailureSocket.address();
-        console.info('UDP server listening on %s:%s', address.address, address.port);
-        // Publish MQTT message that UDP server has started
-        globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.taskFailureServerStatusTopic'), 'start');
+
+  // Handler for UDP server startup event
+  globals.udpServerTaskFailureSocket.on('listening', () => {
+    var address = globals.udpServerTaskFailureSocket.address();
+    console.log('UDP server listening on %s:%s', address.address, address.port);
+    // Publish MQTT message that UDP server has started
+    globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.taskFailureServerStatusTopic'), 'start');
+  });
+
+  // Handler for UDP error event
+  globals.udpServerTaskFailureSocket.on('error', () => {
+    var address = globals.udpServerTaskFailureSocket.address();
+    console.log('UDP server error on %s:%s', address.address, address.port);
+    // Publish MQTT message that UDP server has reported an error
+    globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.taskFailureServerStatusTopic'), 'error');
+  });
+
+  // Main handler for UDP messages relating to failed tasks
+  globals.udpServerTaskFailureSocket.on('message', function(message, remote) {
+    var msg = message.toString().split(';');
+    console.log('%s: Task "%s" failed, associated with app "%s', msg[0], msg[1], msg[2], msg[3]);
+
+    // Post to Slack when a task has failed
+    globals.slack.send({
+      text: 'Failed task: "' + msg[1] + '", linked to app "' + msg[2] + '".',
+      channel: globals.slackTaskFailureChannel,
+      username: msg[0],
+      icon_emoji: ':ghost:'
     });
 
-    // Handler for UDP error event
-    globals.udpServerTaskFailureSocket.on('error', function(message, remote) {
-        var address = globals.udpServerTaskFailureSocket.address();
-        console.error('UDP server error on %s:%s', address.address, address.port);
-        // Publish MQTT message that UDP server has reported an error
-        globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.taskFailureServerStatusTopic'), 'error');
-    });
-
-    // Main handler for UDP messages relating to failed tasks
-    globals.udpServerTaskFailureSocket.on('message', function(message, remote) {
-        var msg = message.toString().split(';');
-        console.info('%s: Task "%s" failed, associated with app "%s', msg[0], msg[1], msg[2], msg[3]);
-
-        // Post to Slack when a task has failed
-        globals.slack.send({
-            text: 'Failed task: "' + msg[1] + '", linked to app "' + msg[2] + '".',
-            channel: globals.slackTaskFailureChannel,
-            username: msg[0],
-            icon_emoji: ':ghost:'
-        });
-
-        // Publish MQTT message when a task has failed
-        globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.taskFailureTopic'), msg[1]);
-    });
+    // Publish MQTT message when a task has failed
+    globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.taskFailureTopic'), msg[1]);
+  });
 
 };
 
@@ -47,54 +48,56 @@ module.exports.udpInitTaskErrorServer = function () {
 // --------------------------------------------------------
 module.exports.udpInitSessionConnectionServer = function () {
 
-    // Handler for UDP server startup event
-    globals.udpServerSessionConnectionSocket.on('listening', function(message, remote) {
-        var address = globals.udpServerSessionConnectionSocket.address();
-        console.info('UDP server listening on %s:%s', address.address, address.port);
+  // Handler for UDP server startup event
+//  globals.udpServerSessionConnectionSocket.on('listening', () => {
+  globals.udpServerSessionConnectionSocket.on('listening', function(message, remote) {
+    var address = globals.udpServerSessionConnectionSocket.address();
+    console.info('UDP server listening on %s:%s', address.address, address.port);
 
-        // Publish MQTT message that UDP server has started
-        globals.mqttClient.publish('qliksense/butler/session_server', 'start');
+    // Publish MQTT message that UDP server has started
+    globals.mqttClient.publish('qliksense/butler/session_server', 'start');
+  });
+
+  // Handler for UDP error event
+//  globals.udpServerSessionConnectionSocket.on('error', () => {
+  globals.udpServerSessionConnectionSocket.on('error', function(message, remote) {
+    var address = globals.udpServerSessionConnectionSocket.address();
+    console.error('UDP server error on %s:%s', address.address, address.port);
+    // Publish MQTT message that UDP server has reported an error
+    globals.mqttClient.publish('qliksense/butler/session_server', 'error');
+  });
+
+  // Main handler for UDP messages relating to session and connection events
+  globals.udpServerSessionConnectionSocket.on('message', function(message, remote) {
+    var msg = message.toString().split(';');
+    console.info('%s: %s for user %s/%s', msg[0], msg[1], msg[2], msg[3]);
+
+    // Send Slack message when session starts/stops, or a connection open/close
+    globals.slack.send({
+      text: msg[1] + ' for user ' + msg[2] + '/' + msg[3],
+      channel: globals.slackLoginNotificationChannel,
+      username: msg[0],
+      icon_emoji: ''
     });
 
-    // Handler for UDP error event
-    globals.udpServerSessionConnectionSocket.on('error', function(message, remote) {
-        var address = globals.udpServerSessionConnectionSocket.address();
-        console.error('UDP server error on %s:%s', address.address, address.port);
-        // Publish MQTT message that UDP server has reported an error
-        globals.mqttClient.publish('qliksense/butler/session_server', 'error');
-    });
+    // Handle session events
+    if (msg[1] == 'Start session') {
+      globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.sessionStartTopic'), msg[0] + ': ' + msg[2] + '/' + msg[3]);
+    }
 
-    // Main handler for UDP messages relating to session and connection events
-    globals.udpServerSessionConnectionSocket.on('message', function(message, remote) {
-        var msg = message.toString().split(';');
-        console.info('%s: %s for user %s/%s', msg[0], msg[1], msg[2], msg[3]);
+    if (msg[1] == 'Stop session') {
+      globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.sessionStopTopic'), msg[0] + ': ' + msg[2] + '/' + msg[3]);
+    }
 
-        // Send Slack message when session starts/stops, or a connection open/close
-        globals.slack.send({
-            text: msg[1] + ' for user ' + msg[2] + '/' + msg[3],
-            channel: globals.slackLoginNotificationChannel,
-            username: msg[0],
-            icon_emoji: ''
-        });
+    // Handle connection events
+    if (msg[1] == 'Open connection') {
+      globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.connectionOpenTopic'), msg[0] + ': ' + msg[2] + '/' + msg[3]);
+    }
 
-        // Handle session events
-        if (msg[1] == 'Start session') {
-            globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.sessionStartTopic'), msg[0] + ': ' + msg[2] + '/' + msg[3]);
-        }
+    if (msg[1] == 'Close connection') {
+      globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.connectionCloseTopic'), msg[0] + ': ' + msg[2] + '/' + msg[3]);
+    }
 
-        if (msg[1] == 'Stop session') {
-            globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.sessionStopTopic'), msg[0] + ': ' + msg[2] + '/' + msg[3]);
-        }
-
-        // Handle connection events
-        if (msg[1] == 'Open connection') {
-            globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.connectionOpenTopic'), msg[0] + ': ' + msg[2] + '/' + msg[3]);
-        }
-
-        if (msg[1] == 'Close connection') {
-            globals.mqttClient.publish(globals.config.get('Butler.mqttConfig.connectionCloseTopic'), msg[0] + ': ' + msg[2] + '/' + msg[3]);
-        }
-
-    });
+  });
 
 };
