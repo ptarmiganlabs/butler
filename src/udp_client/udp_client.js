87c24c34cc8cd14a82c6fea865e5bc0ad124e634
mountaindude <mountaindude@ptarmiganlabs.com>
2016-06-26 08:08:04 +0200

    Fixed ESLint errors

--- src/udp_client/udp_client.js
+++ src/udp_client/udp_client.js
@@ -5,38 +5,38 @@ var yargs = require('yargs');
 
 // Parse command line parameters
 var argv = yargs.usage('Butler UDP client test app', {
-  'about': {
-    description: 'This app sends messages to the UDP server(s) built into Butler (or any other UDP server)',
-    required: false,
-    alias: 'a',
-  },
-  'ip': {
-    description: 'IP address of UDP server message will be sent to',
-    required: true,
-    alias: 'i',
-  },
-  'port': {
-    description: 'Port on UDP server',
-    required: true,
-    number: true,
-    alias: 'p'
-  },
-  'msg': {
-    description: 'Message to send',
-    required: false,
-    default: 'Test message',
-    alias: 'm'
-  }
+    'about': {
+        description: 'This app sends messages to the UDP server(s) built into Butler (or any other UDP server)',
+        required: false,
+        alias: 'a'
+    },
+    'ip': {
+        description: 'IP address of UDP server message will be sent to',
+        required: true,
+        alias: 'i'
+    },
+    'port': {
+        description: 'Port on UDP server',
+        required: true,
+        number: true,
+        alias: 'p'
+    },
+    'msg': {
+        description: 'Message to send',
+        required: false,
+        default: 'Test message',
+        alias: 'm'
+    }
 }).argv;
 
 
-var client = dgram.createSocket({type:"udp4", reuseAddr:true});
+var client = dgram.createSocket({type:'udp4', reuseAddr:true});
 //sock.send(8000, "localhost", buf, 0, buf.length);
 var msg = new Buffer (argv.msg.toString());
 
 //udpServerSessionConnection.bind(9998);
 client.send(msg, 0, msg.length, argv.port, argv.ip, function(err, bytes) {
     if (err) throw err;
-    console.log('UDP message sent to ' + argv.ip +':'+ argv.port);
+    console.info('UDP message sent to ' + argv.ip +':'+ argv.port + ', ' +  bytes + ' bytes.');
     client.close();
 });
