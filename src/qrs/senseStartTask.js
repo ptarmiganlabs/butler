7249ba68227f28b074474c88a5f66107953e9662
mountaindude <mountaindude@ptarmiganlabs.com>
2016-06-26 08:26:39 +0200

    ESLint stuff
    - Tweaked ESLint configuration
    - Fixed ESLint errors

--- src/qrs/senseStartTask.js
+++ src/qrs/senseStartTask.js
@@ -1,14 +1,13 @@
-
+var globals = require('../globals');
 
 // Function for starting Sense task, given its task ID (as it appears in the QMC task list)
 module.exports.senseStartTask = function (taskId) {
-  qrs.post( '/qrs/task/' + taskId + '/start')
-    .then( function ( data) {
-      console.info('return value: ', data );
+    globals.qrs.post( '/qrs/task/' + taskId + '/start')
+        .then( function ( data) {
+            console.info('return value: ', data );
 
-    }, function ( err ) {
-      console.error( 'An error occurred: ', err);
-
-    }
-  )
-}
+        }, function ( err ) {
+            console.error( 'An error occurred: ', err);
+        }
+    );
+};
--- src/qrsUtil/senseStartTask.js
+++ src/qrsUtil/senseStartTask.js
@@ -1,17 +1,14 @@
-
-
+var globals = require('../globals');
 
 // Function for starting Sense task, given its task ID (as it appears in the QMC task list)
 module.exports.senseStartTask = function (taskId) {
-  var globals = require('../globals');
+    globals.qrs.post( '/qrs/task/' + taskId + '/start')
+        .then( function ( data) {
+            console.info('return value: ', data );
 
-  globals.qrs.post( '/qrs/task/' + taskId + '/start')
-    .then( function ( data) {
-      console.info('return value: ', data );
+        }, function ( err ) {
+            console.error( 'An error occurred: ', err);
 
-    }, function ( err ) {
-      console.error( 'An error occurred: ', err);
-
-    }
-  )
-}
+        }
+    );
+};
