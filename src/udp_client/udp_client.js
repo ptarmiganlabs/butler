fcde08345a9877648f3e08bcbc10fe687b667190
Göran Sander <goran@ptarmiganlabs.com>
2018-06-21 17:21:00 +0200

    Adapted to syntax used by latest yargs version

--- src/udp_client/udp_client.js
+++ src/udp_client/udp_client.js
@@ -2,37 +2,32 @@ var dgram = require('dgram');
 var yargs = require('yargs');
 
 // Parse command line parameters
-var argv = yargs.usage('Butler UDP client test app', {
-    'about': {
-        description: 'This app sends messages to the UDP server(s) built into Butler (or any other UDP server)',
-        required: false,
-        alias: 'a'
-    },
-    'ip': {
-        description: 'IP address of UDP server message will be sent to',
-        required: true,
-        alias: 'i'
-    },
-    'port': {
-        description: 'Port on UDP server',
-        required: true,
-        number: true,
-        alias: 'p'
-    },
-    'msg': {
-        description: 'Message to send',
-        required: false,
-        default: 'Test message',
-        alias: 'm'
-    }
-}).argv;
+var argv = yargs
+    .usage('Usage: node $0 [options]\n\nThis app sends messages to the UDP server(s) built into Butler (or any other UDP server)')
 
+    .alias('a', 'about')
+    .alias('i', 'ip')
+    .alias('p', 'port')
+    .alias('m', 'msg')
+    .default({
+        m: 'Test message'
+    })
+    .describe('i', 'IP address of UDP server message will be sent to')
+    .describe('p', 'Port on UDP server')
+    .describe('m', 'Message to send')
+    .demandOption(['i', 'p'])
+    .help('h')
+    .alias('h', 'help')
+    .argv;
 
-var client = dgram.createSocket({type:'udp4', reuseAddr:true});
-var msg = new Buffer (argv.msg.toString());
+var client = dgram.createSocket({
+    type: 'udp4',
+    reuseAddr: true
+});
+var msg = new Buffer(argv.msg.toString());
 
-client.send(msg, 0, msg.length, argv.port, argv.ip, function(err, bytes) {
+client.send(msg, 0, msg.length, argv.port, argv.ip, function (err, bytes) {
     if (err) throw err;
-    console.info('UDP message sent to ' + argv.ip +':'+ argv.port + ', ' +  bytes + ' bytes.');
+    console.info('UDP message sent to ' + argv.ip + ':' + argv.port + ', ' + bytes + ' bytes.');
     client.close();
 });
