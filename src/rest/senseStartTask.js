f963e362b635df0dee770f093f1e0cc8623a4c3b
Göran Sander <goran@ptarmiganlabs.com>
2021-03-18 16:16:40 +0100

    5.2 RC1

--- src/rest/senseStartTask.js
+++ src/rest/senseStartTask.js
@@ -9,6 +9,8 @@ var qrsUtil = require('../qrs_util');
 var logRESTCall = require('../lib/logRESTCall').logRESTCall;
 const errors = require('restify-errors');
 
+var kvstore = require('../rest/keyValueStore');
+
 /**
  * @swagger
  *
@@ -25,6 +27,27 @@ const errors = require('restify-errors');
  *         required: true
  *         type: string
  *         example: "210832b5-6174-4572-bd19-3e61eda675ef"
+ *       - name: extraInfo
+ *         description: |
+ *           Object used to pass additional info related to the reload task being started.
+ *           Currently it's possible to pass in a key-value pair that will be stored in Butler's KV store.
+ *           If Butler's key-value store is not enabled, any key-value information passed in this parameter will simply be ignored.
+ *
+ *           This parameter uses a generic JSON/object format (type + payload).
+ *           It's thus possible to add new integrations in future Butler versions.
+ *         in: body
+ *         type: array
+ *         items:
+ *           type: object
+ *           minItems: 0
+ *           properties:
+ *             type:
+ *               type: string
+ *             payload:
+ *               type: object
+ *         example:
+ *           - type: keyvaluestore
+ *             payload: {namespace: MyFineNamespace, key: AnImportantKey, value: TheValue, ttl: 1000}
  *     responses:
  *       201:
  *         description: Task successfully started.
@@ -39,8 +62,8 @@ const errors = require('restify-errors');
  *       500:
  *         description: Internal error.
  *
- */ 
-module.exports.respondPUT_senseStartTask = function (req, res, next) {
+ */
+module.exports.respondPUT_senseStartTask = async function (req, res, next) {
     logRESTCall(req);
     // TODO: Add task exists test. Return error if not.
 
@@ -49,11 +72,32 @@ module.exports.respondPUT_senseStartTask = function (req, res, next) {
             // Required parameter is missing
             res.send(new errors.MissingParameterError({}, 'Required parameter missing'));
         } else {
+            // Check if there is a message body. If there is, process all the items in it.
+            if (req.body) {
+                for (const item of req.body) {
+                    if (item.type == 'keyvaluestore') {
+                        if (item.payload.namespace && item.payload.key && item.payload.value) {
+                            // We have all data needed to create a KV pair in Butler's KV store.
+                            // First make sure it's enabled...
+                            if (globals.config.get('Butler.restServerEndpointsEnable.keyValueStore')) {
+                                // Store KV pair
+                                await kvstore.addKeyValuePair(item.payload.namespace, item.payload.key, item.payload.value, item.payload.ttl);
+                            } else {
+                                globals.logger.warn('STARTTASK: Trying to store key-value data, but KV store is not enabled.');
+                            }
+                        } else {
+                            // Missing KV data
+                            globals.logger.warn('STARTTASK: Trying to store key-value data, but method call is missing some KV fields..');
+                        }
+                    }
+                }
+            }
+
             // Use data in request to start Qlik Sense task
             globals.logger.verbose(`STARTTASK: Starting task: ${req.params.taskId}`);
 
             qrsUtil.senseStartTask.senseStartTask(req.params.taskId);
-            res.send(201, {'taskId': req.params.taskId});
+            res.send(201, { taskId: req.params.taskId });
             next();
         }
     } catch (err) {
