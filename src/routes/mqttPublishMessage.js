a92bdf1d593a2beefda4127dc2b988573504ce27
Göran Sander <goran@ptarmiganlabs.com>
2021-10-13 09:49:39 +0200

    fix!: MQTT message now taken from body
    Fixes #217

--- src/routes/mqttPublishMessage.js
+++ src/routes/mqttPublishMessage.js
@@ -3,56 +3,22 @@ const httpErrors = require('http-errors');
 // Load global variables and functions
 const globals = require('../globals');
 const { logRESTCall } = require('../lib/logRESTCall');
+const { apiPutMqttMessage } = require('../api/mqttPublishMessage');
 
-/**
- * @swagger
- *
- * /v4/mqttpublishmessage:
- *   put:
- *     description: |
- *       Publish a MQTT message.
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: message
- *         description: Message pubslihed to MQTT
- *         in: body
- *         schema:
- *           type: object
- *           required:
- *             - topic
- *             - message
- *           properties:
- *             topic:
- *               type: string
- *               example: qliksense/new_data_notification/sales
- *             message:
- *               type: string
- *               description: The message is a generic text string and can thus contain anything that can be represented in a string, including JSON, key-value pairs, plain text etc.
- *               example: dt=20201028
- *     responses:
- *       201:
- *         description: MQTT message successfully published.
- *       400:
- *         description: Required parameter missing.
- *       500:
- *         description: Internal error.
- *
- */
 // eslint-disable-next-line consistent-return
-function handler(request, reply) {
+function handlerPutMqttMessage(request, reply) {
     try {
         logRESTCall(request);
 
         if (globals.mqttClient) {
             try {
-                if (request.query.topic === undefined || request.query.message === undefined) {
+                if (request.body.topic === undefined || request.body.message === undefined) {
                     // Required parameter is missing
                     reply.send(httpErrors(400, 'Required parameter missing'));
                 } else {
                     // Use data in request to publish MQTT message
                     if (globals.mqttClient) {
-                        globals.mqttClient.publish(request.query.topic, request.query.message);
+                        globals.mqttClient.publish(request.body.topic, request.body.message);
                     }
 
                     return request.body;
@@ -87,7 +53,6 @@ module.exports = async (fastify, options) => {
         globals.config.get('Butler.restServerEndpointsEnable.mqttPublishMessage')
     ) {
         globals.logger.debug('Registering REST endpoint PUT /v4/mqttpublishmessage');
-
-        fastify.put('/v4/mqttpublishmessage', handler);
+        fastify.put('/v4/mqttpublishmessage', apiPutMqttMessage, handlerPutMqttMessage);
     }
 };
