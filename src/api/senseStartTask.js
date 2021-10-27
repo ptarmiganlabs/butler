4c7368ceb4add6923d12656d2b5cca28fb1e7c5a
Göran Sander <goran@ptarmiganlabs.com>
2021-10-27 14:31:25 +0200

    docs: Fix docs for /v4/reloadtask/:taskId/start
    Fixes #265

--- src/api/senseStartTask.js
+++ src/api/senseStartTask.js
@@ -2,7 +2,7 @@ const apiPutStartTask = {
     schema: {
         summary: 'Start a Qlik Sense task.',
         description:
-            "An optional array of zero or more objects can be passed in the message body. It is used to pass additional info related to the reload task being started.\nCurrently it's possible to pass in a key-value pair that will be stored in Butler's KV store.\nIf Butler's key-value store is not enabled, any key-value information passed in this parameter will simply be ignored.\nUse type=keyvaluestore to send one or more KV pairs to the KV store.\nSetting TTL=0 disables the TTL feature, i.e. the KV pair will not expire.\n\nThis parameter uses a generic JSON/object format (type + payload).\nIt's thus possible to add new integrations in future Butler versions.",
+            "An optional array of zero or more objects can be passed in the message body. It is used to pass additional info related to the reload task being started.\n\nCurrently supported values in this array are:\n\n- A key-value pair that will be stored in Butler's KV store. If Butler's key-value store is not enabled, any key-value information passed in this parameter will simply be ignored.\nSetting `ttl=0` disables the TTL feature, i.e. the KV pair will not expire.\n- A task identified by its taskId that should be started.\n- Tasks identified by tags set on tasks in the QMC.\n- Tasks identified by custom properties set in the QMC.\n\nThis parameter uses a generic JSON/object format (type + payload).\nIt's thus possible to add new integrations in future Butler versions.",
         params: {
             type: 'object',
             properties: {
@@ -37,33 +37,105 @@ const apiPutStartTask = {
                     },
                     payload: {
                         type: 'object',
-                        example: {
-                            namespace: 'MyFineNamespace',
-                            key: 'AnImportantKey',
-                            value: 'TheValue',
-                            ttl: 1000,
-                        },
                     },
                 },
             },
+            example: [
+                {
+                    type: 'keyvaluestore',
+                    payload: { namespace: 'MyFineNamespace', key: 'AnImportantKey', value: 'TheValue', ttl: 10000 },
+                },
+                {
+                    type: 'starttaskid',
+                    payload: {
+                        taskId: '7552d9fc-d1bb-4975-9a38-18357de531ea',
+                    },
+                },
+                {
+                    type: 'starttaskid',
+                    payload: {
+                        taskId: 'fb0f317d-da91-4b86-aafa-0174ae1e8c8',
+                    },
+                },
+                {
+                    type: 'starttasktag',
+                    payload: {
+                        tag: 'startTask1',
+                    },
+                },
+                {
+                    type: 'starttasktag',
+                    payload: {
+                        tag: 'startTask2',
+                    },
+                },
+                {
+                    type: 'starttaskcustomproperty',
+                    payload: {
+                        customPropertyName: 'taskGroup',
+                        customPropertyValue: 'tasks2',
+                    },
+                },
+            ],
         },
         response: {
-            201: {
+            200: {
                 description: 'Task successfully started.',
                 type: 'object',
-                properties: {
-                    tasksStarted: {
-                        taskId: {
-                            type: 'string',
-                            example: '210832b5-6174-4572-bd19-3e61eda675ef',
-                        },
+                example: {
+                    tasksId: {
+                        started: [
+                            {
+                                taskId: 'e3b27f50-b1c0-4879-88fc-c7cdd9c1cf3e',
+                                taskName: 'Reload task of App1',
+                            },
+                            {
+                                taskId: '7552d9fc-d1bb-4975-9a38-18357de531ea',
+                                taskName: 'Reload task of App2',
+                            },
+                            {
+                                taskId: 'fb0f317d-da91-4b86-aafa-0174ae1e8c8f',
+                                taskName: 'Reload task of App3',
+                            },
+                        ],
+                        invalid: [],
                     },
-                    tasksInvalid: {
-                        taskId: {
-                            type: 'string',
-                            example: '210832b5-6174-4572-bd19-3e61eda675ef',
+                    tasksTag: [
+                        {
+                            taskId: 'e3b27f50-b1c0-4879-88fc-c7cdd9c1cf3e',
+                            taskName: 'Reload task of App1',
                         },
-                    },
+                        {
+                            taskId: '7552d9fc-d1bb-4975-9a38-18357de531ea',
+                            taskName: 'Reload task of App2',
+                        },
+                        {
+                            taskId: 'fb0f317d-da91-4b86-aafa-0174ae1e8c8f',
+                            taskName: 'Reload task of App3',
+                        },
+                        {
+                            taskId: 'fb0f317d-da91-4b86-aafa-0174ae1e8c8f',
+                            taskName: 'Reload task of App3',
+                        },
+                    ],
+                    tasksCPs: [
+                        {
+                            taskId: 'e3b27f50-b1c0-4879-88fc-c7cdd9c1cf3e',
+                            taskName: 'Reload task of App1',
+                        },
+                        {
+                            taskId: '7552d9fc-d1bb-4975-9a38-18357de531ea',
+                            taskName: 'Reload task of App2',
+                        },
+                        {
+                            taskId: 'fb0f317d-da91-4b86-aafa-0174ae1e8c8f',
+                            taskName: 'Reload task of App3',
+                        },
+                        {
+                            taskId: '7552d9fc-d1bb-4975-9a38-18357de531ea',
+                            taskName: 'Reload task of App2',
+                        },
+                    ],
                 },
             },
             400: {
